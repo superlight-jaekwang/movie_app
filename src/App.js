b20b8a486e6e2e297aa2cc4d008cac047706d8d6
김재광 <hargreajk@gmail.com>
2019-05-04 21:59:48 +0900

    update3

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   }
 
   _callApi = () => {
-    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
     .then(res => res.json())
     .then(json => json.data.movies)
     .catch(err => console.log(err)) 
@@ -43,8 +43,9 @@ class App extends Component {
   }
 
   render() {
+    const { movies } = this.state
     return (
-      <div className="App">
+      <div className={movies ? "App" : "App--loading"}>
         {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
